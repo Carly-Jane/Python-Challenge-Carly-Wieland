045dfe6e3f98800ce2be29d9b5cf7cf038ea2307
Carly-Jane <101085083+Carly-Jane@users.noreply.github.com>
2022-04-04 18:44:05 +0800

    PyBank Foundations

--- PyBank_Folder/PyBank.py
+++ PyBank_Folder/PyBank.py
@@ -21,3 +21,28 @@
 #Greatest Decrease in Profits: Sep-2013 ($-2196167)
 
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+# let's go baby
+
+Import os
+Import csv
+
+#Empty lists for variables to be counted in. Quick mafths.
+
+Months = []
+Total = []
+largest_increase = []
+largest_decrease = []
+
+# Connect CSV file 
+bank_data_csv = os.path.join(budget_data.csv)
+
+With open(bank_data_csv) as csvfile:
+csvreader = csv.reader(csvfile, delimiter=’,’)
+
+# skip header row
+csv_header = next(csvreader, none)
+
+# alright alright alright
+# *** The total number of months included in the dataset ***
+# Loop through each row and append source data values to variables
